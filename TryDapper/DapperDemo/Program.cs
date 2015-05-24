128a6af4ad0033e1075b7f839a27e1dd56e474eb
yavor <iavorsk@gmail.com>
2015-05-24 17:03:05 +0300

    Added GetOrderDynamic method

--- TryDapper/DapperDemo/Program.cs
+++ TryDapper/DapperDemo/Program.cs
@@ -14,14 +14,15 @@ namespace DapperDemo
     {
         static void Main()
         {
-            var order = GetOrder(10297);
+            //var order = GetOrder(10297);
+            var order = GetOrderDynamic(10297);
 
             Console.WriteLine();
         }
 
         public static Order GetOrder(int id)
         {
-            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthwindDB"].ToString()))
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
             {
                 connection.Open();
 
@@ -31,6 +32,41 @@ namespace DapperDemo
 
                 return order;
             }
+        }
+
+        public static Order GetOrderDynamic(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Orders WHERE OrderID = @ID";
+
+                var dynamicObject = connection.Query(sql, new { ID = id }).FirstOrDefault();
+
+                return new Order 
+                {
+                    CustomerID = dynamicObject.CustomerID,
+                    EmployeeID = dynamicObject.EmployeeID,
+                    Freight = dynamicObject.Freight,
+                    OrderDate = dynamicObject.OrderDate,
+                    OrderID = dynamicObject.OrderID,
+                    RequiredDate = dynamicObject.RequiredDate,
+                    ShipAddress = dynamicObject.ShipAddress,
+                    ShipCity = dynamicObject.ShipCity,
+                    ShipCountry = dynamicObject.ShipCountry,
+                    ShipName = dynamicObject.ShipName,
+                    ShippedDate = dynamicObject.ShippedDate,
+                    ShipPostalCode = dynamicObject.ShipPostalCode,
+                    ShipRegion = dynamicObject.ShipRegion,
+                    ShipVia = dynamicObject.ShipVia
+                }; 
+            }
+        }
+
+        private static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["NorthwindDB"].ToString();
         }
     }
 }
