1eda2bc6acefbd73d4449645385e0e6dacd4d968
yavor <iavorsk@gmail.com>
2015-05-24 16:47:49 +0300

    Introduced Models/Order.cs; first method; added connection string; installed Dapper;

---
+++ TryDapper/DapperDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperDemo.Models;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var order = GetOrder(10297);
+
+            Console.WriteLine();
+        }
+
+        public static Order GetOrder(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthwindDB"].ToString()))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Orders WHERE OrderID = @ID";
+
+                var order = connection.Query<Order>(sql, new { ID = id }).FirstOrDefault();
+
+                return order;
+            }
+        }
+    }
+}
